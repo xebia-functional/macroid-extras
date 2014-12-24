513ff9e4a153a055bfa27439d130abf76559cd6f
Juan Pedro Moreno <juanpmorest@gmail.com>
2014-12-24 15:15:36 +0100

    Update Versions.scala

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,7 +3,7 @@ object Versions {
   val appV = "0.1-SNAPSHOT"
   val scalaV = "2.11.4"
   val androidPlatformV = "android-21"
-  val androidV = "21.0.3"
+  val androidV = "21.0.0"
   val androidPluginV = "1.3.13"
   val macroidV = "2.0.0-M3"
   val akkaV = "2.3.6"
