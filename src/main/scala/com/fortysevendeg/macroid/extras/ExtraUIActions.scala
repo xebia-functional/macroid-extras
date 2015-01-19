2b57ac10c697420cb1a4108e3cb308456c020310
franciscodr <francisco.d@47deg.com>
2015-01-19 23:47:14 +0100

    Addresses some code review comments made by Raul

--- src/main/scala/com/fortysevendeg/macroid/extras/ExtraUIActions.scala
+++ src/main/scala/com/fortysevendeg/macroid/extras/ExtraUIActions.scala
@@ -24,13 +24,15 @@ object ExtraActions {
   def aStartActivityForResult[T <: Activity](result: Int)(implicit c: ActivityContext, m: Manifest[T]): Unit =
     c.get.startActivityForResult(new Intent(c.get, m.runtimeClass), result)
 
-  def aStartActivityFromComponentName[T <: Activity](componentName: ComponentName)(implicit c: ActivityContext): Unit = {
+  def aStartActivityFromComponentName[T <: Activity](componentName: ComponentName)
+      (implicit c: ActivityContext): Unit = {
     val intent = new Intent()
     intent.setComponent(componentName)
     c.get.startActivity(intent)
   }
 
-  def aStartActivityFromComponentNameForResult[T <: Activity](componentName: ComponentName, result: Int)(implicit c: ActivityContext): Unit = {
+  def aStartActivityFromComponentNameForResult[T <: Activity](componentName: ComponentName, result: Int)
+      (implicit c: ActivityContext): Unit = {
     val intent = new Intent()
     intent.setComponent(componentName)
     c.get.startActivityForResult(intent, result)
@@ -46,84 +48,64 @@ object ExtraActions {
 
 object ExtraResources {
 
-  def resGetBoolean(resourceId: Int)(implicit c: AppContext): Boolean = c.get.getResources.getBoolean(resourceId)
-  
-  def resGetBoolean(resource: String)(implicit c: AppContext): Option[Boolean] = {
-    val resourceId = c.get.getResources.getIdentifier(resource, "boolean", c.get.getPackageName)
+  private def resGetResource[T, O](resource: String, resourceType: String)(f: (AppContext, Int) => O)
+      (implicit c: AppContext): Option[O] = {
+    val resourceId = c.get.getResources.getIdentifier(resource, resourceType, c.get.getPackageName)
+
     resourceId match {
-      case res if res == 0 => None
-      case _ => Some(c.get.getResources.getBoolean(resourceId))
+      case 0 => None
+      case _ => Some(f(c, resourceId))
     }
   }
 
+  def resGetBoolean(resourceId: Int)(implicit c: AppContext): Boolean = c.get.getResources.getBoolean(resourceId)
+
+  def resGetBoolean(resource: String)(implicit c: AppContext): Option[Boolean] =
+    resGetResource(resource, "boolean")((c, resourceId) => c.get.getResources.getBoolean(resourceId))
+
   def resGetColor(resourceId: Int)(implicit c: AppContext): Int = c.get.getResources.getColor(resourceId)
-  
-  def resGetColor(resource: String)(implicit c: AppContext): Option[Int] = {
-    val resourceId = c.get.getResources.getIdentifier(resource, "color", c.get.getPackageName)
-    resourceId match {
-      case res if res == 0 => None
-      case _ => Some(c.get.getResources.getColor(resourceId))
-    }
-  }
+
+  def resGetColor(resource: String)(implicit c: AppContext): Option[Int] =
+    resGetResource(resource, "color")((c, resourceId) => c.get.getResources.getColor(resourceId))
 
   def resGetDimension(resourceId: Int)(implicit c: AppContext): Float = c.get.getResources.getDimension(resourceId)
 
-  def resGetDimension(resource: String)(implicit c: AppContext): Option[Float] = {
-    val resourceId = c.get.getResources.getIdentifier(resource, "dimen", c.get.getPackageName)
-    resourceId match {
-      case res if res == 0 => None
-      case _ => Some(c.get.getResources.getDimension(resourceId))
-    }
-  }
-  
+  def resGetDimension(resource: String)(implicit c: AppContext): Option[Float] =
+    resGetResource(resource, "dimen")((c, resourceId) => c.get.getResources.getDimension(resourceId))
+
   def resGetDrawable(resourceId: Int)(implicit c: AppContext): Drawable = c.get.getResources.getDrawable(resourceId)
 
-  def resGetDrawable(resource: String)(implicit c: AppContext): Option[Drawable] = {
-    val resourceId = c.get.getResources.getIdentifier(resource, "drawable", c.get.getPackageName)
-    resourceId match {
-      case res if res == 0 => None
-      case _ => Some(c.get.getResources.getDrawable(resourceId))
-    }
-  }
+  def resGetDrawable(resource: String)(implicit c: AppContext): Option[Drawable] =
+    resGetResource(resource, "drawable")((c, resourceId) => c.get.getResources.getDrawable(resourceId))
 
   def resGetIdentifier(resource: String, resourceType: String)(implicit c: AppContext): Option[Int] = {
     val resourceId = c.get.getResources.getIdentifier(resource, resourceType, c.get.getPackageName)
     resourceId match {
-      case res if res == 0 => None
+      case 0 => None
       case _ => Some(resourceId)
     }
   }
 
   def resGetInteger(resourceId: Int)(implicit c: AppContext): Int = c.get.getResources.getInteger(resourceId)
 
-  def resGetInteger(resource: String)(implicit c: AppContext): Option[Int] = {
-    val resourceId = c.get.getResources.getIdentifier(resource, "integer", c.get.getPackageName)
-    resourceId match {
-      case res if res == 0 => None
-      case _ => Some(c.get.getResources.getInteger(resourceId))
-    }
-  }
-  
-  def resGetResourcePackageName(resourceId: Int)(implicit c: AppContext): String = 
-    c.get.getResources.getResourcePackageName(resourceId)
-  
-  def resGetString(resourceId: Int)(implicit c: AppContext): String = c.get.getResources.getString(resourceId)
-  
-  def resGetString(resource: String)(implicit c: AppContext): Option[String] = {
-    val resourceId = c.get.getResources.getIdentifier(resource, "string", c.get.getPackageName)
-    resourceId match {
-      case res if res == 0 => None
-      case _ => Some(c.get.getResources.getString(resourceId))
-    }
-  }
+  def resGetInteger(resource: String)(implicit c: AppContext): Option[Int] =
+    resGetResource(resource, "integer")((c, resourceId) => c.get.getResources.getInteger(resourceId))
 
-  def resGetString(resourceId: Int, formatArgs: String*)(implicit c: AppContext): String = 
+  def resGetResourcePackageName(resourceId: Int)(implicit c: AppContext): String =
+    c.get.getResources.getResourcePackageName(resourceId)
+
+  def resGetString(resourceId: Int)(implicit c: AppContext): String = c.get.getResources.getString(resourceId)
+
+  def resGetString(resource: String)(implicit c: AppContext): Option[String] =
+    resGetResource(resource, "string")((c, resourceId) => c.get.getResources.getString(resourceId))
+
+  def resGetString(resourceId: Int, formatArgs: String*)(implicit c: AppContext): String =
     c.get.getResources.getString(resourceId, formatArgs)
 
   def resGetString(resource: String, formatArgs: String*)(implicit c: AppContext): Option[String] = {
-    val resourceId = c.get.getResources.getIdentifier(resource, "strings", c.get.getPackageName)
+    val resourceId = c.get.getResources.getIdentifier(resource, "string", c.get.getPackageName)
     resourceId match {
-      case res if res == 0 => None
+      case 0 => None
       case _ => Some(c.get.getResources.getString(resourceId, formatArgs))
     }
   }
