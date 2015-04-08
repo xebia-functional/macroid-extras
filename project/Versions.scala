376a701882486dffd92569d5383795160d526ce5
Javi Pacheco <javielinux@gmail.com>
2015-04-08 09:12:40 +0200

    Tweaks for ViewPager and more

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,8 +2,8 @@ object Versions {
 
   val appV = "snapshot-0.2"
   val scalaV = "2.11.4"
-  val androidPlatformV = "android-21"
-  val androidV = "21.0.0"
+  val androidPlatformV = "android-22"
+  val androidV = "22.0.0"
   val androidPluginV = "1.3.13"
   val macroidV = "2.0.0-M3"
   val akkaV = "2.3.6"
