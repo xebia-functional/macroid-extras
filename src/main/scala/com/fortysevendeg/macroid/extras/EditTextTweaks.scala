a982f27456dfdef93217811f1bb4b40a0a94eae8
Javi Pacheco <javi@47deg.com>
2016-04-02 14:02:37 +0200

    EditTextTweaks converted to object

--- src/main/scala/com/fortysevendeg/macroid/extras/EditTextTweaks.scala
+++ src/main/scala/com/fortysevendeg/macroid/extras/EditTextTweaks.scala
@@ -24,7 +24,7 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import macroid.{ ContextWrapper, Tweak }
 
-class EditTextTweaks {
+object EditTextTweaks {
   type W = EditText
 
   def etAddTextChangedListener(onChanged: (String, Int, Int, Int) ⇒ Unit) = Tweak[W] { view ⇒
